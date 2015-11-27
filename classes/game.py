ee10ea59c6ac8749422b6e555e99273fb43c1540
Justin Sofia <justin.sofia@bba12.mccombs.utexas.edu>
2015-11-27 14:10:26 -0600

    adds input validation and exception handling for column selection

--- classes/game.py
+++ classes/game.py
@@ -14,15 +14,35 @@ class Game:
         # print(self.board+'\n'+self.current_player+", you're up!")
         print(self.board)
         print(self.current_player+", you're up! ")
-        column_choice = input("what'll it be? ")
+        column_choice = self.get_column_choice()
+        if type(column_choice) != int:
+            raise ValueError("custom error: column_choice returned by get_column_choice was not an int!! could be caused by recursion in origin method")
         print("your choice was...")
         print(column_choice)
         self.place_checker(column=column_choice)
-        win = True if self.check_for_win() else False
-        # win = True if column_choice=="bitch" else False
+        # win = True if self.check_for_win() else False
+
+        # haxx for now
+        win = True if column_choice=="exit" else False
         if not win:
           self.toggle_players()
           self.make_move()
+
+    def get_column_choice(self):
+        # while True:       #fancier, probably better way
+            # try:
+            #     x = int(input("Enter a column choice: "))
+            #     break
+            # except ValueError:
+            #     print("Oops! You entered an invalid type. Try again\n")
+        print("What column what you like to place your piece?\n")
+        column_choice = input("Valid choices are columns 0 through 5: ")
+        try:
+            column_choice = int(column_choice)
+            return column_choice if 0 <= column_choice <= 5 else self.get_column_choice()
+        except ValueError:
+            print("{} is not a valid column choice. Please enter a value between 0 and 5".format(column_choice))
+            return self.get_column_choice()
 
     def place_checker(self, column):
         print("you're putting the checker in column {}".format(column))
--- connectfour.py
+++ connectfour.py
@@ -1,4 +1,4 @@
-from classes import board, game
+from classes import board, game, exceptions
 
 b = board.Board()
 
@@ -7,7 +7,10 @@ b = board.Board()
 g = game.Game(board=b)
 
 #game loop
-# perhaps this belongs in the game class
+# as this is procedural logic I believe it ought to go here in the main class
+# however I'm open to it eventually or potentially being handled by the game
+# class for simpliciity's ske
+#
 # while not g.check_for_win():
 #     print('game loop entered')
 #     break
