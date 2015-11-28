642a7f1307d3a4256626968d8a7cfcc2ce9faffd
Justin Sofia <justin.sofia@bba12.mccombs.utexas.edu>
2015-11-27 21:30:28 -0600

    removes extraneous comments from connectfour.py

--- connectfour.py
+++ connectfour.py
@@ -5,12 +5,3 @@ b = board.Board()
 # b.print_board()
 
 g = game.Game(board=b)
-
-#game loop
-# as this is procedural logic I believe it ought to go here in the main class
-# however I'm open to it eventually or potentially being handled by the game
-# class for simpliciity's ske
-#
-# while not g.check_for_win():
-#     print('game loop entered')
-#     break
