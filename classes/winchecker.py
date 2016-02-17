84e94c53832639b111fa72c149844d839d1a58d4
Justin Sofia <justin.sofia@bba12.mccombs.utexas.edu>
2016-02-17 15:41:12 -0600

    fixes issue #3 through adding left-to-right bottom-to-top checking

--- classes/winchecker.py
+++ classes/winchecker.py
@@ -8,7 +8,8 @@ class WinChecker:
         results = []
         results.append(self.check_win_horizontally())
         results.append(self.check_win_vertically())
-        results.append(self.check_win_diagonally())
+        results.append(self.check_win_diagonally_1())
+        results.append(self.check_win_diagonally_2())
         try:
             results.index(True)
             # if the above statement does not generate a ValueError, then a win exists
@@ -69,7 +70,7 @@ class WinChecker:
 
         return win
 
-    def check_win_diagonally(self):
+    def check_win_diagonally_1(self):
         win = False
 
         for i in range(self.board.cols):
@@ -90,6 +91,34 @@ class WinChecker:
                         # win checking progresses from top to bottom
                         for i in range(len(rows)):
                             rows[i] += 1
+                        continue
+                except IndexError:
+                    # we've hit the end of this col, break the loop and move to the next col
+                    break
+
+        return win
+
+    def check_win_diagonally_2(self):
+        win = False
+
+        for i in range(self.board.cols):
+            col = i
+            cols = [col, col+1, col+2, col+3]
+            rows = [5, 4, 3, 2]
+            while not win:
+                try:
+                    # seq will represent the current 4 squares being examined
+                    seq = []
+                    for i in range(len(rows)):
+                        # append current 4 squares to seq list
+                        seq.append(self.board.board[rows[i]][cols[i]])
+                    if self.check_equal(seq):
+                        return True
+                    else:
+                        # increment each value in rows, this is how the diagonal
+                        # win checking progresses from top to bottom
+                        for i in range(len(rows)):
+                            rows[i] -= 1
                         continue
                 except IndexError:
                     # we've hit the end of this col, break the loop and move to the next col
